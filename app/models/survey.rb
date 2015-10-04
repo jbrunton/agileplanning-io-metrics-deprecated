eb3b98192aae7aa83b916032bf4bf45f3f28cb72
John Brunton <jbrunton@zipcar.com>
2015-10-04 17:51:45 +0100

    #8 display responses for surveys

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,12 +3,10 @@ class Survey < ActiveRecord::Base
   has_many :responses
   has_many :answers, through: :responses
 
-  RATINGS = {':)' => 1.0, ':|' => 0.5, ':(' => 0.0}
-
   def analyze
     answers_by_question = answers.group_by { |answer| answer.question }
     ratings_by_question = answers_by_question.map do |question, answers|
-      [question, answers.map{ |answer| RATINGS[answer.answer] }]
+      [question, answers.map{ |answer| answer.rating }.compact]
     end.to_h
     ratings_by_question.map do |question, ratings|
       [question, ratings.reduce(:+) / ratings.length]
