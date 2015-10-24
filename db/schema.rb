88a5bbcc13cfd09b65fb72be0b5d5842f2ff3391
John Brunton <jbrunton@zipcar.com>
2015-10-24 14:49:19 +0100

    #36 scaffolding for teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005193909) do
+ActiveRecord::Schema.define(version: 20151024134845) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "response_id"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20151005193909) do
 
   create_table "surveys", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
