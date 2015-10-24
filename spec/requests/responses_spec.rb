89c42b1ab08723abed8dd5f32873ae6c79fb31ba
John Brunton <jbrunton@zipcar.com>
2015-10-24 16:38:14 +0100

    #38 fix failing test

--- spec/requests/responses_spec.rb
+++ spec/requests/responses_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Responses", type: :request do
   describe "GET /responses" do
     it "works! (now write some real specs)" do
-      survey = Survey.create!(title: 'Some Survey')
+      survey = create(:survey)
       get survey_responses_path(survey)
       expect(response).to have_http_status(200)
     end
